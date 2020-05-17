defcabc94e1ed60ddd3a7179b0f50844d8622a53
ricfrank <riccardo.franconi@gmail.com>
2020-05-17 16:49:08 +0200

    Fetch from githut: sort and filter issues

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -2,7 +2,7 @@ defmodule CliTest do
   use ExUnit.Case
   doctest Issues
 
-  import Issues.CLI, only: [parse_arg: 1]
+  import Issues.CLI, only: [parse_arg: 1, sort_into_descending_order: 1]
 
   test ":help returned by option parsing with -h and --help options" do
     assert parse_arg(["-h", "anything"]) == :help
@@ -16,5 +16,16 @@ defmodule CliTest do
 
   test "count is the default if two values are given" do
     assert parse_arg(["user", "project"]) == {"user", "project", 4}
+  end
+
+  test "sort descending orders" do
+    result = sort_into_descending_order(fake_created_at_list(["c", "a", "b"]))
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+    assert issues == ~w{ c b a }
+  end
+
+  defp fake_created_at_list(values) do
+    for value <- values,
+    do: %{"created_at" => value, "other_data" => "xxx"}
   end
 end
